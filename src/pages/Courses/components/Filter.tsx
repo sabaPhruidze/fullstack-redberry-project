9dd15117f390d2bc4789f2ff1421bf454741afad
Saba <sabaphruidze01@gmail.com>
2026-04-11 14:27:26 +0400

    filters left part is visually built now I will start fetching necessary data

--- src/pages/Courses/components/Filter.tsx
+++ src/pages/Courses/components/Filter.tsx
@@ -4,6 +4,8 @@ import DESIGN from "../../../assets/icons/courses/Icon Set=Design.svg";
 import BUSINESS from "../../../assets/icons/courses/Icon Set=Business.svg";
 import DATA_SCIENCE from "../../../assets/icons/courses/Icon Set=Data Science.svg";
 import MARKETING from "../../../assets/icons/courses/Icon Set=Marketing.svg";
+
+import INSTRUCTOR from "../../../assets/images/catalog/instructor.jpg";
 const Filter = () => {
   const CATEGORIES = [
     {
@@ -32,6 +34,70 @@ const Filter = () => {
       name: "Marketing",
     },
   ];
+  const TOPICS = [
+    {
+      id: 1,
+      name: "React",
+    },
+    {
+      id: 2,
+      name: "TypeScript",
+    },
+    {
+      id: 3,
+      name: "Phyton",
+    },
+    {
+      id: 4,
+      name: "UX/UI",
+    },
+    {
+      id: 5,
+      name: "Figma",
+    },
+    {
+      id: 6,
+      name: "JavaScript",
+    },
+    {
+      id: 7,
+      name: "Node.js",
+    },
+    {
+      id: 8,
+      name: "Machine Learning",
+    },
+    {
+      id: 9,
+      name: "Seo",
+    },
+    {
+      id: 10,
+      name: "Analytics",
+    },
+  ];
+  const INSTRUCTORS = [
+    {
+      id: 1,
+      img: INSTRUCTOR,
+      name: "Marilyn Mango",
+    },
+    {
+      id: 2,
+      img: INSTRUCTOR,
+      name: "Ryan Dorwart",
+    },
+    {
+      id: 3,
+      img: INSTRUCTOR,
+      name: "Roger Calzoni",
+    },
+    {
+      id: 4,
+      img: INSTRUCTOR,
+      name: "Zain Philips",
+    },
+  ];
   return (
     <div className="mt-[34px] w-[309px] h-[914px]">
       <div className="w-full h-[48px] flex flex-row items-center justify-between">
@@ -46,7 +112,7 @@ const Filter = () => {
         </div>
       </div>
       <div className="w-full h-[179px] mt-[32px]">
-        <h3 className="text-[#666666] font-[500] text-[18px] leading-[100%]">
+        <h3 className="text-[#666666] font-[500] text-[18px] leading-[100%] w-full h-[22px]">
           Categories
         </h3>
         <div className="mt-[24px] w-full h-[133px] flex flex-row flex-wrap gap-[8px]">
@@ -60,11 +126,57 @@ const Filter = () => {
                 alt={`${item.name} icon`}
                 className="w-[24px] h-[24px]"
               />
-              <h4 className="text-[#666666] font-[500] text-[16px] leading-[24px]">
+              <h4 className="text-[#666666] font-[500] leading-[24px]">
                 {item.name}
               </h4>
             </div>
           ))}
+        </div>
+      </div>
+      <div className="w-full h-[229px] mt-[56px]">
+        <h3 className="w-full h-[22px] text-[#666666] font-[500] text-[18px] leading-[100%]">
+          Topics
+        </h3>
+        <div className="mt-[24px] w-full h-[133px] flex flex-row flex-wrap gap-[8px]">
+          {TOPICS.map((item) => (
+            <div
+              key={item.id}
+              className="px-[12px] py-[8px] gap-[10px] flex flex-row bg-white rounded-[12px]"
+            >
+              <h4 className="text-[#666666] font-[500] leading-[24px]">
+                {item.name}
+              </h4>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="w-[179px] h-[254px] mt-[56px] gap-[24px] flex flex-col">
+        <h3 className="text-[#666666] font-[500] text-[18px] leading-[100%] w-[179px] h-[22px]">
+          Instructor
+        </h3>
+        <div className="flex flex-col w-[179px] h-[208px] gap-[8px]">
+          {INSTRUCTORS.map((item) => (
+            <div
+              key={item.id}
+              className=" px-[12px] py-[8px] gap-[10px] flex flex-row bg-white rounded-[12px]"
+            >
+              <img
+                src={item.img}
+                alt={`${item.name} icon`}
+                className="w-[24px] h-[24px]"
+              />
+              <h4 className="text-[#666666] font-[500] leading-[24px]">
+                {item.name}
+              </h4>
+            </div>
+          ))}
+        </div>
+        <div className="border-t-[#ADADAD] border-t w-full h-[36px] ">
+          <div className="mt-[16px] w-full h-[20px]">
+            <p className="text-[#999999] font-[500] leading-[100%] text-[14px]">
+              0 Filters Active
+            </p>
+          </div>
         </div>
       </div>
     </div>
